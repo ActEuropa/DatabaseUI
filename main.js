f449e5608bdc847edf7da301d7bee3450671a99c
karmaecrivain94 <karmaecrivain94@users.noreply.github.com>
2017-04-18 15:30:52 +0200

    Adjusting to run on heroku

--- main.js
+++ main.js
@@ -3,38 +3,38 @@ var subdomain = require('express-subdomain');
 var app = express()
 
 app.use(express.static('public'));
+var hRouter = express.Router();
+var dbRouter = express.Router();
 
+//DATABASE ROUTING
 
+//!!! IGNORING SUBDOMAIN RELATED STUFF FOR TESTING PURPOSES ON HEROKU. !!!
+//!!! REPLACE "hRouter" IN THIS SECTION BY "dbRouter" FOR PRODUCTION   !!!
+//app.use(subdomain('database', dbRouter));
 
-//DATABASE ROUTER
-
-var dbRouter = express.Router(); 
-app.use(subdomain('database', dbRouter));
-
-dbRouter.get("/", function(req, res, next) {
-    res.sendFile("views/index.ejs", { root : __dirname})
+hRouter.get("/", function(req, res, next) {
+    res.render("views/index.ejs")
 })
-dbRouter.get("/person/:name", function(req, res, next) {
+hRouter.get("/person/:name", function(req, res, next) {
     console.log("Person=" + req.params.name)
     res.render('PoliticianPage.ejs', {person_name: req.params.name});
 })
-dbRouter.get("/party/:country/:name", function(req, res, next) {
+hRouter.get("/party/:country/:name", function(req, res, next) {
     console.log("Country=" + req.params.country)
     console.log("Party=" + req.params.name)
-    res.sendFile("views/PoliticalParty.ejs", { root : __dirname})
+    res.render("views/PoliticalParty.ejs")
 })
-dbRouter.get("/country/:name", function(req, res, next) {
+hRouter.get("/country/:name", function(req, res, next) {
     console.log("Country=" + req.params.name)
-    res.sendFile("public/html/CountryPage.html", { root : __dirname})
+    res.render("public/html/CountryPage.html")
 })
 
-//MAIN PAGE ROUTER
-var hRouter = express.Router();
-app.use(hRouter);
-//TODO add website root
-hRouter.get("/", function(req, res, next) {
-    res.send("website root");
-})
+//MAIN PAGE ROUTING
+//!!! UNCOMMENT THIS SECTION FOR PRODUCTION !!!
+//app.use(hRouter);
+//hRouter.get("/", function(req, res, next) {
+//    res.send("website root");
+//})
 
 //404 page
 app.use(function (req, res, next) {
