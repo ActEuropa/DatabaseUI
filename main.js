5485ed819b287af995c2b7ec7532d2c64108cd9c
karmaecrivain94 <karmaecrivain94@users.noreply.github.com>
2017-04-19 13:00:24 +0200

    Removing everything related to subdomains

--- main.js
+++ main.js
@@ -3,38 +3,23 @@ var subdomain = require('express-subdomain');
 var app = express()
 
 app.use(express.static('public'));
-var hRouter = express.Router();
-var dbRouter = express.Router();
 
-//DATABASE ROUTING
-
-//!!! IGNORING SUBDOMAIN RELATED STUFF FOR TESTING PURPOSES ON HEROKU. !!!
-//!!! REPLACE "hRouter" IN THIS SECTION BY "dbRouter" FOR PRODUCTION   !!!
-//app.use(subdomain('database', dbRouter));
-
-hRouter.get("/", function(req, res, next) {
+app.get("/", function(req, res, next) {
     res.render("index.ejs");
 })
-hRouter.get("/person/:name", function(req, res, next) {
+app.get("/person/:name", function(req, res, next) {
     console.log("Person=" + req.params.name)
     res.render('PoliticianPage.ejs', {person_name: req.params.name});
 })
-hRouter.get("/party/:country/:name", function(req, res, next) {
+app.get("/party/:country/:name", function(req, res, next) {
     console.log("Country=" + req.params.country)
     console.log("Party=" + req.params.name)
     res.render("PoliticalParty.ejs");
 })
-hRouter.get("/country/:name", function(req, res, next) {
+app.get("/country/:name", function(req, res, next) {
     console.log("Country=" + req.params.name)
     res.render("CountryPage.ejs");
 })
-
-//MAIN PAGE ROUTING
-//!!! UNCOMMENT THIS SECTION FOR PRODUCTION !!!
-//hRouter.get("/", function(req, res, next) {
-//    res.send("website root");
-//})
-app.use(hRouter);
 
 //404 page
 app.use(function (req, res, next) {
