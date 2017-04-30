cafddeaa9bb0c9a02f8392913cb764187d320e19
karmaecrivain94 <karmaecrivain94@users.noreply.github.com>
2017-05-01 01:19:44 +0200

    Implementing (functional!) blog system

---
+++ _posts/What does a european test page look like.md
@@ -0,0 +1,17 @@
+{{{
+  "title":"What does a european test page look like?",
+  "heroimage":"http://image.slidesharecdn.com/presentacineu-141117162201-conversion-gate02/95/the-european-union-creation-1-638.jpg?cb=1416241500",
+  "tags": ["European Union", "Brexit ", "Politics", "Activism","European Parliament"],
+  "category": "javascript",
+  "date": "04-18-2016",
+  "author":"karmaecrivain94",
+  "en":{
+    "title": "What does a european test page look like?",
+    "subtitle": "(That photo reeks of the 90s.) This should appear below the title and also on the home page. In English",
+    "article":"<article>This is just a random shitty article</article>"},
+  "fr":{
+    "title": "A quoi ressemble une page européenne de test?",
+    "subtitle": "(Cette photo pue des années 90) Ce texte devrait appraître sous le titre mais aussi sur la page d'acceuil. En Français",
+    "article":"<article>C'est juste un article random de merde</article>"}
+}}}
+
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var express = require('express')
 var subdomain = require('express-subdomain');
 var cookieParser = require('cookie-parser');
 var i18n = require('i18n');
+var Poet = Poet = require('poet');
 var app = express()
 
 app.use(cookieParser());
@@ -14,8 +15,27 @@ directory: __dirname + '/locales',
 defaultLocale: 'en', 
 cookie: 'lang',
 updateFiles: false });
-
 app.use(i18n.init);
+
+var poet = Poet(app, {
+  posts: __dirname + "/_posts",
+  postsPerPage: 5,
+  metaFormat: 'json'
+});
+poet.watch(function () {
+    console.log("Reloaded poet")
+}).init();
+app.set('view engine', 'markdown');
+poet.addRoute('/blog/:post', function (req, res, next) {
+  var post = poet.helpers.getPost(req.params.post);
+  if (post) {
+    // Do some fancy logging here
+    res.render("Pages/BlogPost.ejs", { lang: i18n.getLocale(req), headerIndex: 0, post: post });
+  } else {
+    res.send(404);
+  }
+}).init();
+
 
 var getPersonData = function(name){
     this.name = "test";
@@ -26,7 +46,8 @@ var getPersonData = function(name){
 ///////////////////////////
 //Home page
 app.get("/", function(req, res, next) {
-    res.render("Pages/Home.ejs", {lang: i18n.getLocale(req), headerIndex: 0});
+    var posts = poet.helpers.getPosts(0,5);
+    res.render("Pages/Home.ejs", {lang: i18n.getLocale(req), headerIndex: 0, posts: posts});
 })
 //Manifesto page
 app.get("/manifesto", function(req, res, next) {
